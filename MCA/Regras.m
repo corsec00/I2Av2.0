8e693b329a202eb017dae2a47dad48e2435f108f
corsec00 <corsec00@gmail.com>
2017-05-06 09:52:03 -0300

    Add files via upload

---
+++ MCA/Regras.m
@@ -0,0 +1,33 @@
+Advanced Penetration Testing for Highly-Secured Environments: The Ultimate Security Guide
+Cover, FM1, i-viii, 7-378
+
+Python Web Penetration Testing Cookbook
+Cover, FM1, i-iii, 1-193
+
+Python Penetration Testing Essentials
+Cover, FM1, i-iii, 5-157
+
+Kali Linux Network Scanning Cookbook
+Cover, FM1, i-iv, 7-428
+
+Mastering Kali Linux for Advanced Penetration Testing
+Cover, FM1, i-v, 13-326
+
+
+OSWP Offensive Security Wireless Attacks (WiFu)	450,00 v3
+OSCP Penetration Testing with Kali (PWK) 800,00 v2014
+OSWE Advanced Web Attacks and Exploitation (AWAE) - Não disponível
+OSCE Cracking the Perimeter (CTP) 1200,00
+
+
+4226 9462
+*0 
+selecao.executiva@ems.com.br
+graphicx
+mathptmx
+	multicol
+refcount
+subcaption
+footnote
+	marvosym
+	amsmath
